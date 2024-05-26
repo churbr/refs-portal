f57162f7be90a75e738f51b54690a082626d1004
Bryan Chu <chu.reybr@gmail.com>
2024-05-27 00:16:17 +0800

    Using only states

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChangePlayerName = (e) => {
+    setSubmitted(false);
+    setEnteredPlayerName(e.target.value);    
+  }
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { submitted ? enteredPlayerName : 'unknown entity' }</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChangePlayerName} value={enteredPlayerName} />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
