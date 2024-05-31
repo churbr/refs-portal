853cd3964df8d5a71efe1046599707aba0e19262
Bryan Chu <chu.reybr@gmail.com>
2024-05-31 23:20:40 +0800

    Using Refs for More Than

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+
+    const timer = useRef();
 
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
 
     const handleStart = () => {
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
 
         setTimerStarted(true);
+    }
+
+    const handleStop = () => {
+        clearTimeout(timer.current);
+        setTimerStarted(false);
     }
 
     return(
@@ -21,7 +28,7 @@ export default function TimerChallenge({ title, targetTime }) {
                 { timerExpired && <p>You lost!</p> }
             </p>
             <p>
-                <button onClick={handleStart}>
+                <button onClick={timerStarted ? handleStop : handleStart}>
                     { timerStarted ?  'Stop' : 'Start'} Challenge
                 </button>
             </p>
